1b1b95a564796da7c741343898fa7fbee5d2547f
iliaspoulakis <iliaspoulakis@github.com>
2020-12-24 01:25:08 +0100

    Fixed various edge cases

--- Various/Drag & drop lanes.lua
+++ Various/Drag & drop lanes.lua
@@ -1,9 +1,11 @@
 --[[
   @author Ilias-Timon Poulakis (FeedTheCat)
   @license MIT
-  @version 1.0.1
+  @version 1.0.2
   @about Runs in the background and lets you change the order of layers using drag & drop
 ]]
+local debug = false
+
 if not reaper.JS_Window_FindChildByID then
     reaper.MB('Please install js_ReaScriptAPI extension', mb_title, 0)
     return
@@ -12,15 +14,16 @@ end
 local main_window = reaper.JS_Window_FindChildByID(reaper.GetMainHwnd(), 1000)
 local _, _, main_y = reaper.JS_Window_GetClientRect(main_window)
 
-local last_edit_flags
-local last_edit_item
+-- View: Toggle displaying labels above/within media items
+local label_height = reaper.GetToggleCommandState(40258) == 1 and 15 or 0
+
+local remeasure = false
+local last_edit_flags = 0
 
 local last_track_t, last_track_b
 local last_item_t, last_item_b
-
-local remeasure = false
-
-local debug = false
+local last_item_h
+local last_item
 
 function print(msg)
     if debug then
@@ -54,7 +57,6 @@ end
 
 function GetItemUnderMouse()
     reaper.PreventUIRefresh(1)
-    reaper.Undo_BeginBlock()
     local sel_items = {}
     -- Save current item selection
     for i = reaper.CountSelectedMediaItems() - 1, 0, -1 do
@@ -65,23 +67,32 @@ function GetItemUnderMouse()
 
     -- Track: Select item under mouse cursor
     reaper.Main_OnCommand(40528, 0)
-    local mouse_item = reaper.GetSelectedMediaItem(0, 0)
-    if mouse_item then
-        reaper.SetMediaItemSelected(mouse_item, false)
-    end
+    local sel_item_cnt = reaper.CountSelectedMediaItems(0)
 
+    -- Get last selected item as its start pos will be closer to the mouse cursor
+    local mouse_item = reaper.GetSelectedMediaItem(0, sel_item_cnt - 1)
+
+    -- If the item was previously selected it has priority
+    for i = 0, sel_item_cnt - 1 do
+        local item = reaper.GetSelectedMediaItem(0, i)
+        for _, sel_item in ipairs(sel_items) do
+            if item == sel_item then
+                mouse_item = sel_item
+                break
+                break
+            end
+        end
+    end
+    reaper.SelectAllMediaItems(0, false)
     -- Restore item selection
     for _, item in ipairs(sel_items) do
         reaper.SetMediaItemSelected(item, true)
     end
-    reaper.Undo_EndBlock('Undo point caused by moving item horizontally (bug)', 0)
     reaper.PreventUIRefresh(-1)
     return mouse_item
 end
 
 function MeasureBounds(item)
-    -- View: Toggle displaying labels above/within media items
-    local label_height = reaper.GetToggleCommandState(40258) == 1 and 14 or 0
     local track = reaper.GetMediaItem_Track(item)
     local track_y = reaper.GetMediaTrackInfo_Value(track, 'I_TCPY')
     local track_h = reaper.GetMediaTrackInfo_Value(track, 'I_WNDH')
@@ -90,13 +101,15 @@ function MeasureBounds(item)
     last_track_t = main_y + track_y
     last_track_b = last_track_t + track_h
     last_item_t = last_track_t + item_y
-    last_item_b = last_item_t + item_h + label_height
+    last_item_b = last_item_t + item_h
+    last_item_h = item_h
     print('Measure: ' .. last_item_b - last_item_t)
+    label_height = reaper.GetToggleCommandState(40258) == 1 and 15 or 0
 end
 
 function GetItemIID(item)
     local _, chunk = reaper.GetItemStateChunk(item, '', true)
-    return tonumber(chunk:match('IID (%d+)\n'))
+    return tonumber(chunk:match('IID (%d+)\n')) or 0
 end
 
 function SetItemIID(item, iid)
@@ -120,24 +133,37 @@ end
 function FixTrackIIDs(track)
     for i = reaper.CountTrackMediaItems(track) - 1, 0, -1 do
         local item = reaper.GetTrackMediaItem(track, i)
-        if item ~= last_edit_item then
+        if item ~= last_item then
             local iid = GetItemIID(item)
             SetItemIID(item, iid + i * 2)
         end
     end
 end
 
-function FixItemTrackIIDs(drag_item)
+function FixItemTrackIIDs_alt(drag_item)
     reaper.Undo_BeginBlock()
     local track = reaper.GetMediaItem_Track(drag_item)
-    local drag_item_offset = 2
-    for i = reaper.CountTrackMediaItems(track) - 1, 0, -1 do
+    local drag_item_offset = 4
+    local item_cnt = reaper.CountTrackMediaItems(track)
+    for i = item_cnt - 1, 0, -1 do
         local item = reaper.GetTrackMediaItem(track, i)
         if item ~= drag_item then
             local iid = GetItemIID(item)
-            SetItemIID(item, iid + i * 2 + drag_item_offset)
+            SetItemIID(item, item_cnt * i * 4 + drag_item_offset)
         else
             drag_item_offset = 0
+        end
+    end
+    reaper.Undo_EndBlock('Prepare track for lanes changes (bugfix?)', -1)
+end
+
+function FixItemTrackIIDs(drag_item)
+    reaper.Undo_BeginBlock()
+    local track = reaper.GetMediaItem_Track(drag_item)
+    for i = 0, reaper.CountTrackMediaItems(track) - 1 do
+        local item = reaper.GetTrackMediaItem(track, i)
+        if item ~= drag_item then
+            SetItemIID(item, i * 15)
         end
     end
     reaper.Undo_EndBlock('Prepare track for lanes changes (bugfix?)', -1)
@@ -147,36 +173,41 @@ function Main()
     local _, _, edit_flags = reaper.GetItemEditingTime2()
     if edit_flags ~= 0 and last_edit_flags == 0 then
         print('Drag!')
-        last_edit_item = GetItemUnderMouse()
-        if last_edit_item then
-            MeasureBounds(last_edit_item)
-            FixItemTrackIIDs(last_edit_item)
+        last_item = GetItemUnderMouse()
+        if last_item then
+            MeasureBounds(last_item)
+            FixItemTrackIIDs(last_item)
         end
     end
 
-    if remeasure and last_edit_item then
-        MeasureBounds(last_edit_item)
+    if remeasure and last_item then
+        MeasureBounds(last_item)
         remeasure = false
     end
 
-    if edit_flags == 4 then
+    if edit_flags == 4 and last_item then
+        if reaper.GetMediaItemInfo_Value(last_item, 'I_LASTH') ~= last_item_h then
+            print('Item height changed...')
+            MeasureBounds(last_item)
+        end
         local mouse_y = select(2, reaper.GetMousePosition())
-        local is_within_track = mouse_y > last_track_t and mouse_y < last_track_b
-        if not is_within_track and last_edit_item then
+        local is_within_track = mouse_y >= last_track_t and mouse_y < last_track_b
+        if not is_within_track and last_item then
             print('Track changed...')
-            MeasureBounds(last_edit_item)
-            FixItemTrackIIDs(last_edit_item)
+            MeasureBounds(last_item)
+            FixItemTrackIIDs(last_item)
         end
 
-        local is_above_item = mouse_y < last_item_t
+        local is_above_item = mouse_y < last_item_t - label_height
         if is_above_item then
+            -- Avoid cursor being on the label of the first lane
             print('Up: ' .. mouse_y .. ' | ' .. last_item_t .. ' ' .. last_item_b)
             -- Item lanes: Move item up one lane (when showing overlapping items in lanes)
             reaper.Main_OnCommand(40068, 0)
             remeasure = true
         end
 
-        local is_below_item = mouse_y > last_item_b
+        local is_below_item = mouse_y > last_item_b + label_height
         if is_below_item then
             print('Down: ' .. mouse_y .. ' | ' .. last_item_t .. ' ' .. last_item_b)
             -- Item lanes: Move item down one lane (when showing overlapping items in lanes)
@@ -187,6 +218,7 @@ function Main()
     end
 
     if edit_flags == 0 and last_edit_flags ~= 0 then
+        last_item = nil
         print('Drop!')
     end
 
