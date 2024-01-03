44a1959bfc74d67aefdc2eea49a490d4a1fecc6a
ipoulakis <iliaspoulakis@github.com>
2024-01-03 12:21:55 +0100

    Updated changelog

--- Adaptive grid/Adaptive grid.lua
+++ Adaptive grid/Adaptive grid.lua
@@ -1,7 +1,7 @@
 --[[
   @author Ilias-Timon Poulakis (FeedTheCat)
   @license MIT
-  @version 1.7.1
+  @version 1.7.2
   @about A fully featured adaptive grid tool for REAPER
   @metapackage
   @provides
@@ -11,5 +11,6 @@
     [main=main,midi_editor] Set grid to * (adaptive).lua
     [main=main,midi_editor] Adjust adaptive grid (mousewheel).lua
   @changelog
-    - Added option for dotted grid
+    - Adjust grid via mousewheel script now also respect grid limits when
+      scrolling through fixed grid divisions (adaptive turned off)
 ]]
