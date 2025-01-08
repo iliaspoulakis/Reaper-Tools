ddcc7839566848346c5a19918316b4f432caf831
iliaspoulakis <iliaspoulakis@github.com>
2025-01-08 15:12:05 +0100

    Fix certain chords being incorrectly displayed as octaves

--- MIDI editor/Lil Chordbox.lua
+++ MIDI editor/Lil Chordbox.lua
@@ -1,12 +1,13 @@
 --[[
   @author Ilias-Timon Poulakis (FeedTheCat)
   @license MIT
-  @version 2.2.1
+  @version 2.3.0
   @provides [main=main,midi_editor] .
   @about Adds a little box to the MIDI editor that displays chord information
   @changelog
-    - Display inversions on chord degrees
-    - Fix option to display chord inversions
+    - Show selected chords only when notes overlap
+    - Show inversions by default
+    - Fix certain chords being incorrectly displayed as octaves
 ]]
 local box_x_offs = 0
 local box_y_offs = 0
@@ -266,7 +267,7 @@ local use_input = reaper.GetExtState(extname, 'input') ~= '0'
 local degree_mode = tonumber(reaper.GetExtState(extname, 'degree_only')) or 3
 
 local sel_mode = tonumber(reaper.GetExtState(extname, 'sel_mode')) or 2
-local use_inversions = reaper.GetExtState(extname, 'inversions') == '1'
+local use_inversions = reaper.GetExtState(extname, 'inversions') ~= '0'
 local use_solfege = reaper.GetExtState(extname, 'solfege') == '1'
 local use_sharps = reaper.GetExtState(extname, 'sharps') == '1'
 local use_sharps_autodetect = reaper.GetExtState(extname, 'sharps_auto') ~= '0'
@@ -391,7 +392,12 @@ function IdentifyChord(notes)
         intervals = {}
         for _, note in ipairs(notes) do
             local diff = note.pitch - root
-            if diff >= 12 then intervals[diff % 12 + 13] = 1 end
+            if diff >= 12 then
+                intervals[diff % 12 + 13] = 1
+            elseif diff > 0 then
+                intervals = {}
+                break
+            end
         end
         -- Create compound chord key string
         local comp_key = '1'
